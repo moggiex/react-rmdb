78a7478f8be67b8b8e910f2a50dfe5f3ae9adc99
Matt Ogborne <mogggiex@gmail.com>
2022-02-22 07:49:37 +0000

    1:08:03

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// Styles 
+import { GlobalStyle } from './GlobalStyle';
+
 const App = () => {
   return (
     <div className="App">
       Start here 2
+      <GlobalStyle />
     </div>
   );
 }
